70c1b2307fccdd17974a8376850906e05e254432
shaggy-axel <kornerus@fjfi.cvut.cz>
2021-08-23 20:21:32 +0300

    Add Applications

--- src/shaggy_axel_web/settings.py
+++ src/shaggy_axel_web/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.users',
+    'apps.blog',
+    'apps.resume',
+    'apps.portfolio',
+    'apps.education',
 ]
 
 MIDDLEWARE = [
