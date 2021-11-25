d337b0dcb38b98915832582adfaeb20363977855
Ruslan <kornerus@fjfi.cvut.cz>
2021-11-25 09:32:26 +0300

    change `venv -> .venv` and delete some lines in run script

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # Create Environment "Virtual Environment file path", "Deploy Flag file path"
-VENV=./venv
-DEPLOY_FLAG=/opt/shaggy-axel/deploy_state.flag
-
-# Create flag file in current dir
-touch $DEPLOY_FLAG
+VENV=./.venv
 
 # Check if not exists VENV, -> create VENV
 if [ ! -d $VENV ]; then
@@ -13,10 +9,6 @@ if [ ! -d $VENV ]; then
     $VENV/bin/pip install -U pip
 fi
 
-`which python3` -m venv $VENV
-
-# Upgrade pip
-$VENV/bin/pip install -U pip
 # Installing requirements from file
 $VENV/bin/pip install -r requirements.txt
 
@@ -24,10 +16,5 @@ $VENV/bin/pip install -r requirements.txt
 $VENV/bin/python src/manage.py migrate
 $VENV/bin/python src/manage.py collectstatic --no-input
 
-
-echo "Run Django"
 $VENV/bin/python src/manage.py runserver 0.0.0.0:8000
-
-# When app will be stoped, remove Deploy Flag
-rm -f ../$DEPLOY_FLAG
-
+echo "Run Django"
