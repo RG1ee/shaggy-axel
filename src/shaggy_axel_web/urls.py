bd9b95f260a16d6133dd479150e010254a89144b
Ruslan <kornerus@fjfi.cvut.cz>
2021-12-02 01:51:29 +0300

    add resume

--- src/shaggy_axel_web/urls.py
+++ src/shaggy_axel_web/urls.py
@@ -18,12 +18,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.resume.views import resume_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),    
     path('api/users/', include('apps.users.api.urls')),
     # path('blogs/', include('apps.blog.api.urls')),
+    path('resume/', resume_view, name="shaggy-resume")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
