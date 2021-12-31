daf69ffd6d77e4f4d1e4262f42d1a5dced0b7258
Ruslan <79697348+shaggy-axel@users.noreply.github.com>
2021-12-31 22:11:09 +0300

    urls refactoring

--- src/shaggy_axel_web/urls.py
+++ src/shaggy_axel_web/urls.py
@@ -1,18 +1,3 @@
-"""shaggy_axel_web URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -23,7 +8,9 @@ from apps.resume.views import resume_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),    
+    path("api/auth/", include(
+        "rest_framework.urls",
+        namespace="rest_framework")),
     path('api/users/', include('apps.users.api.urls')),
     path('resume/', resume_view, name="shaggy-resume"),
     path('blog/', include('apps.blog.urls')),
