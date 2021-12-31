4004233e4d9e64daccaa02e79ef1bd6afa0f70c8
Ruslan <79697348+shaggy-axel@users.noreply.github.com>
2021-12-31 21:57:09 +0300

    blog refactoring

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -7,12 +7,17 @@ STATUS = (
     (1, "Publish")
 )
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(
+        Member, on_delete=models.CASCADE,
+        related_name='blog_posts')
     content = models.TextField()
-    image = models.ImageField("Image", upload_to="blog", default="blog/default/Ruslan_Korneev.PNG", blank=True)
+    image = models.ImageField(
+        "Image", upload_to="blog",
+        default="blog/default/Ruslan_Korneev.PNG", blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -6,6 +6,7 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/index.html'
 
+
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
