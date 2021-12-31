31e45ac49d9393aa2d6b0970612968cbc56e0bf1
Ruslan <79697348+shaggy-axel@users.noreply.github.com>
2021-12-31 22:00:24 +0300

    blog api temp comment all

--- src/apps/blog/admin.py
+++ src/apps/blog/admin.py
@@ -5,7 +5,7 @@ from apps.blog.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status','created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
