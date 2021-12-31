4a6495c97276f38fdf71692bdb8d84506b8ca5b4
Ruslan <79697348+shaggy-axel@users.noreply.github.com>
2021-12-31 22:05:44 +0300

    users api refactoring

--- src/apps/users/api/views.py
+++ src/apps/users/api/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 
-from .serializers import MemberDetailSerializer, MemberListSerializer, MemberRagisterSerializer
+from .serializers import (
+    MemberDetailSerializer,
+    MemberListSerializer,
+    MemberRagisterSerializer)
 
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.views import APIView
