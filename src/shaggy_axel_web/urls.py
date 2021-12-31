eefbcf09cd2dcb4520265967046772fc896ad77c
Ruslan <79697348+shaggy-axel@users.noreply.github.com>
2021-12-31 21:46:24 +0300

    add static

--- src/shaggy_axel_web/urls.py
+++ src/shaggy_axel_web/urls.py
@@ -29,4 +29,10 @@ urlpatterns = [
     path('blog/', include('apps.blog.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
