4532688e25fcd8cdb66525698e221c3cb26288d3
Ruslan <79697348+shaggy-axel@users.noreply.github.com>
2022-01-03 03:55:46 +0300

    use loop and more `time-rows` in template

--- src/apps/timetable/views.py
+++ src/apps/timetable/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def timetable_render_view(request):
-    return render(request, template_name="timetable/timetable.html")
+    work_time_list = (
+        "7.00 - 8.00", "8.00 - 9.00", "9.00 - 10.00",
+        "10.00 - 11.00", "11.00 - 12.00", "12.00 - 13.00",
+        "13.00 - 14.00", "14.00 - 15.00", "15.00 - 16.00",
+        "16.00 - 17.00", "17.00 - 18.00", "18.00 - 19.00",
+        "19.00 - 20.00", "20.00 - 21.00", "21.00 - 22.00")
+    data = {
+        "work_time_list": work_time_list,
+    }
+    return render(request, "timetable/timetable.html", data)
